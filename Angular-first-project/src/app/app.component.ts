df8f20740838b5ab1569f0f5497a154309d208b6
Vallejos Jose Luis <joseluisvallejos1995@gmail.com>
2023-01-28 19:38:57 -0300

    style: :art: Change the first msj.

--- Angular-first-project/src/app/app.component.ts
+++ Angular-first-project/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-raiz',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular-first-project';
+  title = 'Jose Luis Vallejos';
+
+  saludo = 'Hola Youtube';
 }
