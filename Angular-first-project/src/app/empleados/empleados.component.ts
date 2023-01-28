676575360c0e1c733afde1752d60dea54c5a4e99
Vallejos Jose Luis <joseluisvallejos1995@gmail.com>
2023-01-28 20:30:47 -0300

    feat: :art: Primer componente creado JAJAJJA!!!!!

---
+++ Angular-first-project/src/app/empleados/empleados.component.ts
@@ -0,0 +1,17 @@
+//1 Construir la clase
+//2 Crear el decorador encima de la clase
+
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-empleados",
+    templateUrl: "./empleados.component.html",
+    styleUrls: ["./empleados.component.scss"]
+
+})
+
+export class EmpleadosComponent{
+
+
+
+}
