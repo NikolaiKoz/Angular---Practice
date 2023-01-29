35485dd8b1736c84a8365548445b5a0fbadba81a
Vallejos Jose Luis <joseluisvallejos1995@gmail.com>
2023-01-28 21:01:41 -0300

    feat: :zap: Componente inline

--- Angular-first-project/src/app/empleado/empleado.component.ts
+++ Angular-first-project/src/app/empleado/empleado.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+//ESTO ES UN COMPONENTE INLINE
+
 @Component({
   selector: 'app-empleado',
-  templateUrl: './empleado.component.html',
-  styleUrls: ['./empleado.component.scss']
+  // templateUrl: './empleado.component.html',
+  //styleUrls: ['./empleado.component.scss']
+  template: "<p class='empleado'>Este empleado funciona</p><span class='empleado__otro'>Si lo hace</span>",
+  styles: [".empleado__otro{color: purple;};.empleado__otro{background-color: red}"]
 })
+
+
 export class EmpleadoComponent {
 
 }
