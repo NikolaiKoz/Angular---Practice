e616c297047ff664df2e7a5d65266d8726e5420c
Vallejos Jose Luis <joseluisvallejos1995@gmail.com>
2023-01-29 16:44:29 -0300

    feat: :zap: Estudiando directivas con Angular.

---
+++ trabajoDirectivaApp/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styles: ["h1{width: 100%; text-align: center; background-color: orange;}"]
+})
+export class AppComponent {
+  title = 'Directivas con Angular';
+}
